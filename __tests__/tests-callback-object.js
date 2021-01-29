0e99a99ba08cf9488cfda70182ba8c9221b40d62
S0AndS0 <strangerthanbland@gmail.com>
2021-01-28 21:01:29 -0800

    :coffee: Adds transpiled tests for `Callback_Object` class

---
+++ __tests__/tests-callback-object.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+class Test__Callback_Object {
+    /**
+     *
+     */
+    constructor() {
+        this.callback_object = require('../iterator-cascade-callbacks').Callback_Object;
+        this.callback_wrapper = (callback_object, iterator_cascade_callbacks) => {
+            const callback = (value, index_or_key, { callback_object, iterator_cascade_callbacks }, paramaters) => {
+                return [value, index_or_key];
+            };
+            const [value, index_or_key] = iterator_cascade_callbacks.value;
+            const results = callback(value, index_or_key, { callback_object, iterator_cascade_callbacks }, ...callback_object.parameters);
+            if (Array.isArray(results)) {
+                iterator_cascade_callbacks.value = results;
+            }
+            else {
+                iterator_cascade_callbacks.value = [results, index_or_key];
+            }
+        };
+        this.callback_paramaters = ['first', 'second', 'third'];
+    }
+    /**
+     *
+     */
+    runTests() {
+        this.testsConstructor();
+    }
+    /**
+     *
+     */
+    testsConstructor() {
+        test('testsConstructor -> Are object properties assigned as designed?', () => {
+            const name = 'noOpp';
+            const callback_object = new this.callback_object(this.callback_wrapper, name, this.callback_paramaters);
+            expect(callback_object.name).toStrictEqual(name);
+            expect(callback_object.parameters).toStrictEqual(this.callback_paramaters);
+            expect(callback_object.wrapper).toStrictEqual(this.callback_wrapper);
+        });
+        test('testsConstructor -> Do `.paramaters` default to an empty array if undefined?', () => {
+            const name = 'empty_paramaters';
+            /* @ts-ignore */
+            const callback_object = new this.callback_object(this.callback_wrapper, name, undefined);
+            expect(callback_object.parameters).toStrictEqual([]);
+        });
+        // test('testsConstructor ->', () => {});
+    }
+}
+const test__callback_object = new Test__Callback_Object();
+test__callback_object.runTests();
+//# sourceMappingURL=tests-callback-object.js.map
